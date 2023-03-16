337c1e731c44de1dc79d3b29aa9a08bdf87b5e75
Joe Diamand <jdiamand@yahoo.com>
2023-03-16 18:02:25 -0400

    coroutines in caller

--- ToasterLibrary/src/main/java/com/carma/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/carma/toasterlibrary/ToasterMessage.kt
@@ -1,12 +1,23 @@
 package com.carma.toasterlibrary
 
-import android.content.Context
-import android.widget.Toast
 
+import android.util.Log
+
+
+/*
 class ToasterMessage {
     companion object {
         fun show(context: Context, message: String, duration: Int = Toast.LENGTH_SHORT) {
             Toast.makeText(context, message, duration).show()
+        }
+    }
+}
+*/
+class StartAR {
+    companion object {
+        fun start(){
+
+            Log.d("StartAR","Start")
         }
     }
 }
