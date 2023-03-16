c856481bf5ee685a243679f52d430335bebcf4d7
Joe Diamand <jdiamand@yahoo.com>
2023-03-16 19:47:10 -0400

    coroutines in callee

--- ToasterLibrary/src/main/java/com/carma/toasterlibrary/ToasterMessage.kt
+++ ToasterLibrary/src/main/java/com/carma/toasterlibrary/ToasterMessage.kt
@@ -4,8 +4,9 @@ package com.carma.toasterlibrary
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import kotlinx.coroutines.delay
 
-
+/*
 class ToasterMessage {
     companion object {
         fun show(context: Context, message: String, duration: Int = Toast.LENGTH_SHORT) {
@@ -13,13 +14,22 @@ class ToasterMessage {
         }
     }
 }
-
+*/
 class StartAR {
+    enum class STATUS {
+        OK, ERROR
+    }
     companion object {
-        fun start() : String {
-
+        var status = STATUS.OK
+        var ERROR_CODE = "UNKOWN_ERROR"
+        suspend fun start() : String {
+            delay(5000)
             Log.d("StartAR","Start")
-            return "OK"
+            return if (status == STATUS.OK) {
+                "OK"
+            } else {
+                "ERROR : $ERROR_CODE"
+            }
         }
     }
 }
